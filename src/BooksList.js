62a4e6f98a382e986ecd981b2ea14d59a81a1e1f
Ajay Nara <33077370+ajay2696@users.noreply.github.com>
2018-03-11 12:04:47 -0400

    Fix: Changed class component to stateless component and Fixed Thumbnail Issue

--- src/BooksList.js
+++ src/BooksList.js
@@ -1,45 +1,44 @@
-import React,{Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-class BooksList extends Component{
- static propTypes ={
-   books:PropTypes.array.isRequired,
-   onBookShelfChange:PropTypes.func.isRequired
- }
- render(){
-          return (<div>
-                   <ul className="books-grid">
-                     {this.props.books.map((book) =>
-                       (<li key={book.id} className="book">
-                         <div className="book-top">
-                            <picture >
-                              <source srcSet={book.imageLinks.thumbnail}/>
-                              <img src={book.imageLinks.thumbnail} className="book-cover" alt={book.title}/>
-                            </picture>
-                            <button className="book-shelf-changer">
-                                  <select onChange={(e)=>this.props.onBookShelfChange(e,book)} value={book.shelf}>
-                                    <option value="moveTo" disabled="true">Move to</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead"> Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                  </select>
-                            </button>
-                           </div>
-                           <div className="book-title"><strong>{book.title}</strong></div>
+const BooksList =(props) => {
+    return (
+        <div>
+            <ul className="books-grid">
+                {props.books.map((book) =>{
+                    const bookThumbnail = book.imageLinks ? book.imageLinks.thumbnail :"http://via.placeholder.com/128x193?text=No%20Cover";
+                    return (
+                        <li key={book.id} className="book">
+                            <div className="book-top">
+                                <picture>
+                                    <source srcSet={bookThumbnail}/>
+                                    <img src={bookThumbnail} className="book-cover" alt={book.title}/>
+                                </picture>
+                                <button className="book-shelf-changer">
+                                    <select onChange={(e)=>props.onBookShelfChange(e,book)} value={book.shelf}>
+                                        <option value="moveTo" disabled="true">Move to</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead"> Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                    </select>
+                                </button>
+                            </div>
+                            <div className="book-title"><strong>{book.title}</strong></div>
                             {Array.isArray(book.authors)&&(book.authors.map((author) =>(
-                                                            <div key={author}
-                                                                  className="book-authors">
-                                                              <strong>{author}</strong>
-                                                            </div>)
-                                                          ))
-                            }
+                                <div key={author}
+                                    className="book-authors">
+                                    <strong>{author}</strong>
+                                </div>)
+                            ))}
                         </li>
-                        )
-                       )
-                     }
-                    </ul>
-                 </div>);
-         }
-    }
-
-export default BooksList
+                    )})
+                }
+            </ul>
+        </div>
+    );
+}
+BooksList.propTypes = {
+    books :PropTypes.array.isRequired,
+    onBookShelfChange :PropTypes.func.isRequired
+}
+export default BooksList;
