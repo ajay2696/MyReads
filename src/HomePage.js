cea91c95165e30e5a2673b359405a8d986e395fc
Ajay Nara <33077370+ajay2696@users.noreply.github.com>
2018-03-11 12:05:17 -0400

    Fix: Changed class component to Stateless Component

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,35 +1,36 @@
-import React,{Component} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import BooksList from './BooksList';
 import PropTypes from 'prop-types';
-class HomePage extends Component
-{
-static propTypes={
-  selectedBooks:PropTypes.array.isRequired,
-  onBookShelfChange:PropTypes.func.isRequired
+const HomePage=(props) => {
+    const currentlyReadingBooks =props.selectedBooks.filter(book => book.shelf==='currentlyReading');
+    const wantToReadBooks =props.selectedBooks.filter(book => book.shelf==='wantToRead');
+    const alreadyReadbooks =props.selectedBooks.filter(book => book.shelf==='read');
+    return (
+        <div>
+            <header className="list-books-title">
+                <h1>MyReads</h1>
+            </header>
+            <Link to='/search' className="open-search">Search</Link>
+            <div className="bookshelf">
+                <h1 className="bookshelf-title"> Currently Reading </h1>
+                <BooksList books={currentlyReadingBooks}
+                    onBookShelfChange={props.onBookShelfChange} />
+            </div>
+            <div className="bookshelf">
+                <h1 className="bookshelf-title"> Want to Reading </h1>
+                <BooksList books={wantToReadBooks}
+                    onBookShelfChange={props.onBookShelfChange} />
+            </div>
+            <div className="bookshelf">
+                <h1 className="bookshelf-title"> Read </h1>
+                <BooksList books={alreadyReadbooks}
+                    onBookShelfChange={props.onBookShelfChange} />
+            </div>
+        </div>);
 }
-render(){
-          return (<div>
-                    <header className="list-books-title">
-                      <h1>MyReads</h1>
-                    </header>
-                    <Link to='/search' className="open-search">Search</Link>
-                    <div className="bookshelf">
-                      <h1 className="bookshelf-title"> Currently Reading </h1>
-                      <BooksList books={this.props.selectedBooks.filter(book => book.shelf==='currentlyReading')}
-                                  onBookShelfChange={this.props.onBookShelfChange} />
-                    </div>
-                    <div className="bookshelf">
-                      <h1 className="bookshelf-title"> Want to Reading </h1>
-                      <BooksList books={this.props.selectedBooks.filter(book => book.shelf==='wantToRead')}
-                                onBookShelfChange={this.props.onBookShelfChange} />
-                    </div>
-                    <div className="bookshelf">
-                      <h1 className="bookshelf-title"> Read </h1>
-                      <BooksList books={this.props.selectedBooks.filter(book => book.shelf==='read')}
-                                onBookShelfChange={this.props.onBookShelfChange} />
-                    </div>
-                  </div>);
-        }
+HomePage.propTypes ={
+    selectedBooks:PropTypes.array.isRequired,
+    onBookShelfChange:PropTypes.func.isRequired
 }
-export default HomePage
+export default HomePage;
